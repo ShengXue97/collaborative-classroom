41f533c3d2dfee4a671456be79809abe35cb98d5
sunxiuqi-stacked <59859941+sunxiuqi-stacked@users.noreply.github.com>
2020-08-08 12:13:46 +0800

    ignore

---
+++ Downloads/PERSONAL DOCS/SavedGames/collaborative-classroom-master/src/twilio/api/index.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const getToken = async user =>
+  axios.get("https://collaborative-classroom-server.herokuapp.com/token", {
+    params: {
+      user,
+    },
+  });
+  
